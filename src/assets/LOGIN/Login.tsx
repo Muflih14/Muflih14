c752df75cae350f34df50ea91fa655ef9387b25f
Muflih14 <muflihaabd@gmail.com>
2023-01-24 11:27:34 +0100

    refactored codes

--- src/assets/LOGIN/Login.tsx
+++ src/assets/LOGIN/Login.tsx
@@ -1,28 +1,30 @@
 import NavBar from "../NavBar/NavBar";
-import {useNavigate} from 'react-router-dom';
-import Connect from "./connect";
-import './login.css';
+import { useNavigate } from "react-router-dom";
+import "./login.css";
 
 function LogIn() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
- return (
+  return (
     <>
-    <div>
+      <div>
         <NavBar />
-    </div>
-    <div className="content">
-       <h2>Log IN</h2>
+      </div>
+      <div className="content">
+        <h2>Log IN</h2>
 
-    <div className='buttons'>
-       <button className="button"><a onClick={() => navigate('/projects')}>CONNECT TO WALLET</a></button>
-       <Connect />
-       <button className="button"><a href="https://bitcoin.org/en/choose-your-wallet">CREATE A WALLET</a></button>
-    </div>
-
-    </div>
+        <div className="buttons">
+          <button className="button">
+            <a onClick={() => navigate("/projects")}>CONNECT TO WALLET</a>
+          </button>
+          <button className="button">
+            <a href="https://bitcoin.org/en/choose-your-wallet">
+              CREATE A WALLET
+            </a>
+          </button>
+        </div>
+      </div>
     </>
-    );
- }
+  );
+}
 export default LogIn;
