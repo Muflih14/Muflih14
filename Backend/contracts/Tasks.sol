ead787aa7886843249b0ec42e8e4c8d61284c598
Muflih14 <muflihaabd@gmail.com>
2023-02-14 16:56:28 +0100

    update on smart contract

--- Backend/contracts/Tasks.sol
+++ Backend/contracts/Tasks.sol
@@ -3,23 +3,43 @@ pragma solidity ^0.8.9;
 
 // Creating a contract
 contract Tasks {
+    string[] public tasks;
+
     // Defining a structure to store a task
     struct Task {
         string task;
-		bool isDeleted;
-        bool isDone;
+        bool isDeleted;
+        bool isCompleted;
+        bool isImportant;
     }
- 
+
     mapping(address => Task[]) private Users;
+
+    function setTasks(string memory _task) public {
+        tasks.push(_task);
+    }
+
+    // In this function we are just returning the array
+    function getTasks() public view returns (string[] memory) {
+        return tasks;
+    }
+
+    // Here we are returning the length of the tasks array
+    function getTasksLength() public view returns (uint) {
+        uint tasksLength = tasks.length;
+        return tasksLength;
+    }
 
     // Add a task
     function addTask(string calldata _task) external {
-        Users[msg.sender].push(Task({task: _task, isDeleted: false, isDone: false}));
+        Users[msg.sender].push(
+            Task({task: _task, isDeleted: false, isCompleted: false, isImportant: false})
+        );
     }
 
     // Update task status
     function updateStatus(uint256 _taskIndex, bool _status) external {
-        Users[msg.sender][_taskIndex].isDone = _status;
+        Users[msg.sender][_taskIndex].isCompleted = _status;
     }
 
     // Delete a task
